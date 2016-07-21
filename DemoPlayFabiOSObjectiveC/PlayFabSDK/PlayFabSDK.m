da03632255c77e5b58180f9a1c56c68f7020070b
Nguyen Van Thieu B <nguyen.van.thieub@framgia.com>
2016-07-21 09:22:17 +0700

    002 - Add lib PlayFab

---
+++ DemoPlayFabiOSObjectiveC/PlayFabSDK/PlayFabSDK.m
@@ -0,0 +1,13 @@
+//
+//  PlayFabSDK.m
+//  PlayFabSDK
+//
+//  Created by William Burgers on 2/3/16.
+//  Copyright © 2016 PlayFab, Inc. All rights reserved.
+//
+
+#import "PlayFabSDK.h"
+
+@implementation PlayFabSDK
+
+@end
